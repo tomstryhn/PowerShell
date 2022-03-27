c7673f3729af1807d7870d06ab42cd2a1da8bf2d
Tom Stryhn <93223547+tomstryhn@users.noreply.github.com>
2022-03-27 05:08:38 +0200

    Update GitHubLoader.ps1

--- GitHub/GitHubLoader/GitHubLoader.ps1
+++ GitHub/GitHubLoader/GitHubLoader.ps1
@@ -1,7 +1,12 @@
+# Defines the source of the script sources
 $sourceUrl  = 'https://raw.githubusercontent.com/tomstryhn/PowerShell/main/GitHub/GitHubLoader/sources.json'
+
+# Loads the list directly from GitHub and converts it from Json
 $sourceList = (Invoke-WebRequest -Uri $sourceUrl -UseBasicParsing).Content | ConvertFrom-Json
 
+# For each of the functions in the sourcelist
 foreach($script in $sourceList) {
 
+    # Get the code directly form the sourcecode url using the Invoke-WebRequest and then 'loads' the function into the session using Invoke-Expression
     Invoke-Expression -Command ((Invoke-WebRequest -Uri $script.sourcecode -UseBasicParsing).Content)
 }
